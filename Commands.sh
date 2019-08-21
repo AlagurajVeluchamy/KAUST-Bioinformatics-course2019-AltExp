af5115f5d26e85d4beeef5edde623fb7f7dd76b4
Alaguraj Veluchamy <alaguraj.v@gmail.com>
2019-08-22 02:04:09 +0300

    Update Commands.sh

--- Commands.sh
+++ Commands.sh
@@ -1,5 +1,5 @@
-Resource site: https://github.com/CBC-KAUST/RNAseqKAUST2019
+Resource site: https://github.com/CBC-KAUST/AltExpKAUST2019
 ssh velucha@ilogin.ibex.kaust.edu.sa
 srun --time=5:00:00 --mem=16G --nodes=1 -c 16 --pty bash -l
-git clone https://github.com/CBC-KAUST/RNAseqKAUST2019.git
+git clone https://github.com/CBC-KAUST/AltExpKAUST2019.git
 
